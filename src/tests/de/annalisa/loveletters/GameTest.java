c9d9b0ea08240a249a1fb94b41c0755523b30948
Annalisa11 <annalisa.comin@hotmail.com>
2023-10-21 01:33:31 +0200

    deleted test, changed gitignore

--- src/tests/de/annalisa/loveletters/GameTest.java
+++ src/tests/de/annalisa/loveletters/GameTest.java
@@ -15,35 +15,7 @@ class GameTest {
 
     @Test
     void startGame() {
-        // Create a new game
-        Game game = new Game();
 
-        // Create two players with specific cards
-        Player player1 = new Player("Player1");
-        player1.addCardToHand(new Card("Countess", 7, "If you have this card and the King or Prince in your hand, you must discard this card."));
-        player1.addCardToHand(new Card("Priest", 2, "Look at another player's hand"));
-
-        Player player2 = new Player("Player2");
-        player2.addCardToHand(new Card("Handmaid", 4, "Until your next turn, ignore all effects from other players' cards."));
-        player2.addCardToHand(new Card("Priest", 2, "Look at another player's hand"));
-
-        // Add the players to the game
-        game.getPlayers().add(player1);
-        game.getPlayers().add(player2);
-
-        // Create a deck with specific cards
-        game.createDeck();
-        game.getDeck().addCard(new Card("King", 6, "Trade hands with another player your choice"));
-        game.getDeck().addCard(new Card("Prince", 5, "Choose any player (including yourself) to discard his or her hand and draw a new card"));
-        game.getDeck().addCard(new Card("Prince", 5, "Choose any player (including yourself) to discard his or her hand and draw a new card"));
-
-        // Simulate the game
-        game.startGame();
-
-        // Add your assertions here to verify the game results
-        // For example:
-        // assertEquals(player1, game.gameWinner); // Check the expected winner
-        // assertEquals(expectedScore, player1.getScore()); // Check the expected score for player1
     }
 
 }
